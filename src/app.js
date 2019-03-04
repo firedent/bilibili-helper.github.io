bf920f4dc36f3b4353bd635a9158fb3e9014f931
DrowsyFlesh <jjj201200@gmail.com>
2019-03-05 03:08:29 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@ export const dva = {
     config: {
         onError(e) {
             e.preventDefault();
-            console.error(e.message);
+            //console.error(e.message);
         },
     },
     plugins: [
-        require('dva-logger')({
-            predicate: (state, action) => action.type.indexOf('@') === -1 && action.type !== 'image/update' && action.type !== 'image/fetch',
-            collapsed: true,
-        }),
+        //require('dva-logger')({
+        //    predicate: (state, action) => action.type.indexOf('@') === -1 && action.type !== 'image/update' && action.type !== 'image/fetch',
+        //    collapsed: true,
+        //}),
     ],
 };
 
