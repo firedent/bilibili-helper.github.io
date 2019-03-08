785422dcda24f054c0f2dada890c3110e4997811
DrowsyFlesh <jjj201200@gmail.com>
2019-03-08 20:23:51 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 /* global local */
+import _ from 'lodash';
+
 export const dva = {
     config: {
         onError(e) {
@@ -6,11 +8,11 @@ export const dva = {
             //console.error(e.message);
         },
     },
-    plugins: [
+    plugins: _.compact([
         local ? require('dva-logger')({
             predicate: (state, action) => action.type.indexOf('@') === -1 && action.type !== 'image/update' && action.type !== 'image/fetch',
             collapsed: true,
         }) : '',
-    ],
+    ]),
 };
 
