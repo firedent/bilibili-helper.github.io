37589b22e5526a4401a8a5171aad5a6f38eb1274
Zac Yu <me@zacyu.com>
2019-03-23 22:37:31 -0400

    Change display strings.

--- src/components/VoteArea/index.js
+++ src/components/VoteArea/index.js
@@ -168,7 +168,7 @@ class VoteArea extends React.Component {
         return (
             <VoteAreaWrapper>
                 <h3>
-                    新功能投票 ~ VOTE FOR NEW FEATURE
+                    新功能投票 ~ FEATURE REQUESTS
                     <p>为希望添加进助手的功能点赞吧~</p>
                 </h3>
                 <div className="votes-list">
