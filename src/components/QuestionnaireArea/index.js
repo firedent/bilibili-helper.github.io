fc6428ef1903b0a1cafe223c7741aff135211f31
DrowsyFlesh <jjj201200@gmail.com>
2020-11-26 10:22:10 +0800

    更新置顶公告

--- src/components/QuestionnaireArea/index.js
+++ src/components/QuestionnaireArea/index.js
@@ -43,7 +43,7 @@ class QuestionnaireArea extends React.Component {
         return (
             <Wrappr id="announcement">
                 <h3>问卷调查 ~ QUESTIONNAIRE</h3>
-                <p>Bilibili helper 2.0开测啦~全新的POPUP界面，成为你上b站的第一助手。测试文件下载QQ群：677639415</p>
+                <p>Bilibili helper 2.0开测啦~全新的POPUP界面，成为你上b站的第一助手，QQ群：677639415。2.0群里不发布1.x修复版，如需下载1.x测试版文件请加QQ群548321019</p>
                 <p>如果觉得助手用起来还不错，恳请前往 <a href="https://chrome.google.com/webstore/detail/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9%E5%8A%A9%E6%89%8B%EF%BC%9Abilibilicom-%E7%BB%BC%E5%90%88%E8%BE%85%E5%8A%A9%E6%89%A9%E5%B1%95/kpbnombpnpcffllnianjibmpadjolanh?hl=zh-CN">&nbsp;谷歌商店&nbsp;</a> 给予五星好评~从而让更多人知道~</p>
                 <p>关于POPUP页面的问卷，很简短，请参与填写一下~：→ <a href="https://docs.qq.com/form/edit/DUHZ3allZS0ZYQlBN">link</a></p>
                 <p>经过肉肉4天的紧急开发，<a href="https://chrome.google.com/webstore/detail/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9%E9%9F%B3%E4%B9%90%EF%BC%9Abilibilicom-%E8%BE%85%E5%8A%A9%E6%89%A9%E5%B1%95/lhjebpdodpjgfihhaocoepmdojfhknfn">《哔哩哔哩音乐》</a>上架了，之后会增加更过实用功能，这里是一份调差问卷：→<a href="https://docs.qq.com/form/fill/DUGR4bE1rbkJrc1Vm?_w_tencentdocx_form=1">link</a></p>
