45ef5aeb2117f203775684357a6381c09439032d
DrowsyFlesh <jjj201200@gmail.com>
2020-12-28 22:22:17 +0800

    更新内容

--- src/components/QuestionnaireArea/index.js
+++ src/components/QuestionnaireArea/index.js
@@ -43,6 +43,7 @@ class QuestionnaireArea extends React.Component {
         return (
             <Wrappr id="announcement">
                 <h3>问卷调查 ~ QUESTIONNAIRE</h3>
+                <p>哔哩哔哩助手2.0+ 功能清单：<a target="_blank" href="https://shimo.im/docs/ypGHYjwdwrv3hXr9">石墨文档</a></p>
                 <p>视频下载方式：<a target="_blank" href="https://shimo.im/docs/PXgDrXTGcXjtWjVX#anchor-Fnmu">石墨文档</a></p>
                 <p>Bilibili helper 2.0开测啦~全新的POPUP界面，成为你上b站的第一助手，QQ群：677639415。2.0群里不发布1.x修复/测试版本，如需下载1.x修复/测试版文件请加主群548321019</p>
                 <p>如果觉得助手用起来还不错，恳请前往 <a target="_blank" href="https://chrome.google.com/webstore/detail/%E5%93%94%E5%93%A9%E5%93%94%E5%93%A9%E5%8A%A9%E6%89%8B%EF%BC%9Abilibilicom-%E7%BB%BC%E5%90%88%E8%BE%85%E5%8A%A9%E6%89%A9%E5%B1%95/kpbnombpnpcffllnianjibmpadjolanh?hl=zh-CN">&nbsp;谷歌商店&nbsp;</a> 给予五星好评~从而让更多人知道~</p>
